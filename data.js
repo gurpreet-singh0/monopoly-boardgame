682360bcab939ce2d4554ff76c5100e918b5e520
gurpreet-singh0 <122405825+gurpreet-singh0@users.noreply.github.com>
2023-04-12 11:45:13 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,33 @@
+// reviewData array
+window.reviewData = [
+    {
+      name: "John Smith",
+      date: "January 1, 2020",
+      rating: 5,
+      review: "This game is so much fun! I've been playing it for years and I still can't get enough."
+    },
+    {
+      name: "Jane Doe",
+      date: "February 1, 2020",
+      rating: 4,
+      review: "I've only been playing for a few months, but I'm already addicted! Such a great game."
+    },
+    {
+      name: "Bob Jones",
+      date: "March 1, 2020",
+      rating: 3,
+      review: "It's ok, but I'm not as into it as my friends are. It's still fun though."
+    },
+    {
+      name: "Mary Johnson",
+      date: "April 1, 2020",
+      rating: 4,
+      review: "I love this game! I love how easy it is to learn but there's still so much strategy involved."
+    },
+    {
+      name: "Joe Brown",
+      date: "May 1, 2020",
+      rating: 5,
+      review: "This game is an absolute classic! I've been playing it for decades and it's still just as fun as ever."
+    }
+  ];
